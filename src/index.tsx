e5cbbb004d8add1e4a600c9444927c0ea14c67a0
szynek99 <pielat.szymon@gmail.com>
2024-02-20 15:49:42 +0100

    feature: react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,23 @@ import App from './app';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import theme from './theme';
 import '@fontsource/raleway';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>,
 );
