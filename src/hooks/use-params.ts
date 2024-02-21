fae3947d94c491f85aabdc7fa45eff3d7d897651
szynek99 <pielat.szymon@gmail.com>
2024-02-21 15:37:42 +0100

    feature: debounce, connected search

--- src/hooks/use-params.ts
+++ src/hooks/use-params.ts
@@ -1,11 +1,12 @@
 import { useCallback } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-type ParamKeys = 'page' | 'per_page';
+type ParamKeys = 'page' | 'per_page' | 'id';
 
 const defaultParams = {
   page: '1',
   per_page: '5',
+  id: '',
 };
 
 export const useParams = () => {
@@ -14,8 +15,12 @@ export const useParams = () => {
   const getParam = useCallback((key: ParamKeys) => searchParams.get(key) || '', [searchParams]);
 
   const setParam = useCallback(
-    (key: ParamKeys, value: string) => {
-      searchParams.set(key, value);
+    (key: ParamKeys, value?: string) => {
+      if (value) {
+        searchParams.set(key, value);
+      } else {
+        searchParams.delete(key);
+      }
       setSearchParams(searchParams);
     },
     [searchParams, setSearchParams],
@@ -23,9 +28,10 @@ export const useParams = () => {
 
   const getParams = useCallback(() => {
     const params = {} as Record<ParamKeys, string>;
-    for (const key of ['page', 'per_page'] as ParamKeys[]) {
+    for (const key of ['page', 'per_page', 'id'] as ParamKeys[]) {
       params[key] = getParam(key) || defaultParams[key];
     }
+
     return params;
   }, [getParam]);
 
